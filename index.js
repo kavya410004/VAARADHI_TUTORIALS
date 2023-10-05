2189a553f0b101b9fc054acf5cff5113c74667fd
kavya410004 <116058046+kavya410004@users.noreply.github.com>
2023-10-05 21:02:32 +0530

    Add new changes to website

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 document.getElementById('email-button').addEventListener('click', function() {
     window.location.href = 'form.html'; 
 });
+// icons
+document.getElementById('whatsapp-icon').addEventListener('click', function() {
+  alert("Our Whatsapp Number: +919704882298")
+})
 
 // To know the screen size
 const screenWidth = window.innerWidth;
@@ -36,10 +40,61 @@ function handleScroll() {
   }
   
 }
+
+function lastChild(el){
+  console.log(el.childNodes);
+  var last = el.childNodes.length - 1;
+  return el.childNodes[last];
+  
+}
+
+// details of exam onclick
+function displayInfo(el){
+  const element = el;
+  var infoBlock = lastChild(element);
+  if (infoBlock.style.display === 'none')
+  {
+    console.log('working')
+  }
+}
+
 handleScroll();
 // calls handleScroll function when the site is scrolled
 window.addEventListener('scroll', handleScroll);
 
+
+for (var i = 0; i < 4; i++){
+  const element = document.querySelectorAll('.exam-card')[i];
+  element.addEventListener('click',displayInfo);
+}
+
+
+
+// add click event to all .exam-card
+// Get all elements with class "exam-card"
+const examCards = document.querySelectorAll('.exam-card');
+
+// Add click event listeners to each "exam-card" element
+examCards.forEach((card) => {
+    card.addEventListener('click', () => {
+        // Find the card-content element within the clicked "exam-card"
+        const cardContent = card.querySelector('.text-box');
+
+        // Toggle the display of the card-content element
+        if (cardContent.style.display === 'none' || cardContent.style.display === '') {
+            cardContent.style.display = 'block';
+            card.style.height = 'auto';
+        } else {
+            cardContent.style.display = 'none';
+            if(screenWidth <= 576)
+            {
+              card.style.height = '130px';
+            }else{
+              card.style.height = '150px';
+            }
+        }
+    });
+});
 
 
 
